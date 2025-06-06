9dd904808aad234343d66a290d53ecb8e467f9b5
Rolando <143459727+RolandoCamposLoreto@users.noreply.github.com>
2025-06-06 03:40:53 +0000

    agregado al codigo de MergeSort

--- 4. Algoritmos de ordenamiento/4.1 Internos/6. MergeSort.py
+++ 4. Algoritmos de ordenamiento/4.1 Internos/6. MergeSort.py
@@ -1,3 +1,9 @@
 form array import array
 def ordenar(lista):
     tamano_de_lista= len(lista) -1 
+    for posicion_actual in range(0, tamano_de_lista):
+        posicion_menor = posicion_actual
+        nombre_menor = lista[posicion_menor]
+        for posicion_buscar in range(posicion_actual, tamano_de_lista):
+            nombre_buscar = lista[posicion_buscar + 1]
+            if nombre_menor 
